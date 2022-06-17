a2ba922f642a7ac822360bc076f4d596c1b849be
Pollyana <pollyanarocha416@gmail.com>
2022-06-16 22:32:09 -0300

    Avaliar se a senha e correta

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program{
+    static void Main(){
+
+        string senha = "567";
+        string? senhauser;
+        int tentativas = 3;
+        do
+        {
+            Console.Clear();
+            Console.Write("Digite a senha: ");
+            senhauser = Console.ReadLine();
+            tentativas++;
+        } while (senha != senhauser);
+        Console.Clear();
+         if(tentativas>3){
+            Console.Write("Conta Bloqueada");
+        }else{Console.WriteLine($"Senha correta! tentativas: {tentativas}");
+       }
+        
+    }
+}
